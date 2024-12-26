5cc514bf97017b2bab7bfea6da939e9bc2e0d4de
William Grosset <williamhgrosset@gmail.com>
2024-12-26 12:27:55 -0700

    Clean up test suite

--- tests/tests.rs
+++ tests/tests.rs
@@ -2,32 +2,27 @@ use std::collections::HashMap;
 
 use cykling::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn test_regular_grammar() {
+    let mut grammar = HashMap::new();
 
-    #[test]
-    fn test_regular_grammar() {
-        let mut grammar = HashMap::new();
+    grammar.insert("S".to_string(), vec![
+        Rule::NonTerminal("A".to_string(), "B".to_string()),
+    ]);
+    grammar.insert("B".to_string(), vec![
+        Rule::NonTerminal("B".to_string(), "C".to_string()),
+        Rule::Terminal("b".to_string()),
+    ]);
+    grammar.insert("A".to_string(), vec![Rule::Terminal("a".to_string())]);
+    grammar.insert("C".to_string(), vec![Rule::Terminal("c".to_string())]);
 
-        grammar.insert("S".to_string(), vec![
-            Rule::NonTerminal("A".to_string(), "B".to_string()),
-        ]);
-        grammar.insert("B".to_string(), vec![
-            Rule::NonTerminal("B".to_string(), "C".to_string()),
-            Rule::Terminal("b".to_string()),
-        ]);
-        grammar.insert("A".to_string(), vec![Rule::Terminal("a".to_string())]);
-        grammar.insert("C".to_string(), vec![Rule::Terminal("c".to_string())]);
+    assert!(cyk("abc", &grammar, "S"));
+    assert!(!cyk("acb", &grammar, "S"));
+}
 
-        assert!(cyk("abc", &grammar, "S"));
-        assert!(!cyk("acb", &grammar, "S"));
-    }
-
-    #[test]
-    fn test_empty_grammar() {
-        let mut empty_grammar = HashMap::new();
-        empty_grammar.insert("S".to_string(), vec![Rule::Terminal("".to_string())]);
-        assert!(cyk("", &empty_grammar, "S"));
-    }
+#[test]
+fn test_empty_grammar() {
+    let mut empty_grammar = HashMap::new();
+    empty_grammar.insert("S".to_string(), vec![Rule::Terminal("".to_string())]);
+    assert!(cyk("", &empty_grammar, "S"));
 }
