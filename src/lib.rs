9224502e56a3a1462498b35a3d25993d02e2df20
William Grosset <williamhgrosset@gmail.com>
2024-12-26 12:18:29 -0700

    Add test suite

--- src/lib.rs
+++ src/lib.rs
@@ -76,30 +76,3 @@ pub fn cyk(input: &str, grammar: &HashMap<String, Vec<Rule>>, start_symbol: &str
 
     table[0][n - 1].contains(start_symbol)
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_cyk_algorithm() {
-        let mut grammar = HashMap::new();
-
-        grammar.insert("S".to_string(), vec![
-            Rule::NonTerminal("A".to_string(), "B".to_string()),
-        ]);
-        grammar.insert("B".to_string(), vec![
-            Rule::NonTerminal("B".to_string(), "C".to_string()),
-            Rule::Terminal("b".to_string()),
-        ]);
-        grammar.insert("A".to_string(), vec![Rule::Terminal("a".to_string())]);
-        grammar.insert("C".to_string(), vec![Rule::Terminal("c".to_string())]);
-
-        assert!(cyk("abc", &grammar, "S"));
-        assert!(!cyk("acb", &grammar, "S"));
-
-        let mut empty_grammar = HashMap::new();
-        empty_grammar.insert("S".to_string(), vec![Rule::Terminal("".to_string())]);
-        assert!(cyk("", &empty_grammar, "S"));
-    }
-}
